ae2b13036a9e1741f1c088937a2865453774ef4c
Барабаш Максим Евгеньевич <mbarabash2000@gmail.com>
2024-12-02 18:49:23 +0300

    Delete 3.py

--- my_math_package/__init__.py
+++
@@ -1,6 +0,0 @@
-# my_math_package/__init__.py
-# Импортируем класс из модуля calculator
-from .calculator import NumberCalculator
-
-# Можно явно указать, что именно экспортируется
-__all__ = ['NumberCalculator']
