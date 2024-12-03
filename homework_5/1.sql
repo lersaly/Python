b6f5479c6b6f91b6dd104e0da0fbbfc3a31c4721
Барабаш Максим Евгеньевич <mbarabash2000@gmail.com>
2024-12-03 20:20:24 +0300

    Добавлена 5-я работа

---
+++ homework_5/1.sql
@@ -0,0 +1,3 @@
+SELECT product_name, unit_price 
+FROM products
+WHERE unit_price >= 3 AND unit_price < 7;
---
+++ homework_5/2.sql
@@ -0,0 +1,3 @@
+SELECT MIN(unit_price) AS min_price
+FROM products
+WHERE category_id = 1;
